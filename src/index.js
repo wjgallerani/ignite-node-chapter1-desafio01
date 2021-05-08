0203f6fafa91c0d0f882080ca4a5cbf9bc79b295
William Gallerani <williamglrn@gmail.com>
2021-05-07 21:37:23 -0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -25,9 +25,6 @@ function checksExistsUserAccount(request, response, next) {
    return next();
 }
 
-
-
-
 app.post('/users', (request, response) => {
   const { name, username }  = request.body;
  
